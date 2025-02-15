f9302eea9a2843ed1aa19634605353d879855bcb
DIVIJ <divijerode7@gmail.com>
2025-02-16 00:53:03 +0530

    added get in touch

---
+++ server.js
@@ -0,0 +1,39 @@
+// filepath: /C:/Users/DIVIJ/OneDrive/Documents/GitHub/my-portfolio/server.js
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/send-email', (req, res) => {
+    const { name, email, message } = req.body;
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'your-email@gmail.com',
+            pass: 'your-email-password'
+        }
+    });
+
+    const mailOptions = {
+        from: email,
+        to: 'divij.contact@gmail.com',
+        subject: `New message from ${name}`,
+        text: message
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return res.status(500).send(error.toString());
+        }
+        res.status(200).send('Email sent: ' + info.response);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
